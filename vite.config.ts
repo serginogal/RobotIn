15eab65dcc6f72f67796ec3b71a032cea72d5cb8
serginogal <snogal.cm@gmail.com>
2023-02-11 14:07:39 +0900

    Configure @components and @layouts paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// @ts-nocheck
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore:Cannot find module 'path' or its corresponding type declarations.ts(2307)
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // @ts-ignore:Cannot find name '__dirname'.ts(2304)
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/components'),
+    },
+  },
   plugins: [react()],
 })
