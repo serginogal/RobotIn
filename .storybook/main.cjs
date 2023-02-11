becaaba7f120996ac33df1b1ad53a8ca709e4255
serginogal <snogal.cm@gmail.com>
2023-02-11 15:58:02 +0900

    Add @ paths to Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,28 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@services': path.resolve(__dirname, '../src/services'),
+      '@layouts': path.resolve(__dirname, '../src/layouts'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
+      '@views': path.resolve(__dirname, '../src/views'),
+    }
+    return config
+  },
 }
