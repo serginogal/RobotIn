79392de0a31737821990978e9260a1a1487044c7
serginogal <snogal.cm@gmail.com>
2023-02-11 14:11:51 +0900

    Add Logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss'
+
+const Logo = () => {
+  return (
+    <span className={styles.logo}>
+      Robot
+      <span className={styles.logoBolder}>In</span>
+    </span>
+  )
+}
+
+export default Logo
